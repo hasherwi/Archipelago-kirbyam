88c5d0ce3b13a1e383842e1e1ab70e1a6d5a6c72
Harrison Sherwin <harrison.sherwin@gmail.com>
2026-04-14 09:17:17 -0600

    Fix room and switch label formatting for issue 710

--- worlds/kirbyam/test/test_data_normalization.py
+++ worlds/kirbyam/test/test_data_normalization.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ..data import _normalize_gba_rom_address
+from ..data import _normalize_gba_rom_address, format_room_region_label
 
 
 @pytest.mark.parametrize(
@@ -18,3 +18,17 @@ def test_normalize_gba_rom_address_mapped_ranges(raw_addr, expected_offset):
 
 def test_normalize_gba_rom_address_passthrough_for_non_mapped_values():
     assert _normalize_gba_rom_address(0x00F00000) == 0x00F00000
+
+
+@pytest.mark.parametrize(
+    "region_key, expected_label",
+    [
+        ("REGION_MOONLIGHT_MANSION/ROOM_2_BOSS", "Area 2 - Boss Room"),
+        ("REGION_MOONLIGHT_MANSION/ROOM_2_HUB", "Area 2 - Hub Room"),
+        ("REGION_RAINBOW_ROUTE/ROOM_1_HUB_3", "Area 1 - Hub Room 3"),
+        ("REGION_MOONLIGHT_MANSION/ROOM_2_18", "REGION_MOONLIGHT_MANSION/ROOM_2_18"),
+        ("REGION_MOONLIGHT_MANSION/MAIN", "REGION_MOONLIGHT_MANSION/MAIN"),
+    ],
+)
+def test_format_room_region_label(region_key: str, expected_label: str) -> None:
+    assert format_room_region_label(region_key) == expected_label
